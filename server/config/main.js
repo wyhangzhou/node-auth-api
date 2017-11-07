a08a7e0851f610727bff722310ce90d194f63a6a
Yao Wang <wy.hangzhou@gmail.com>
2017-11-07 16:12:06 -0700

    client working

--- server/config/main.js
+++ server/config/main.js
@@ -1,3 +1,8 @@
 module.exports = {
+    // Secret key for JWT signing and encryption
+    'secret': 'super secret passphrase',
+    // Database connection information
+    'database': 'mongodb://test:test@ds117605.mlab.com:17605/node-auth-api',
+    // Setting port for server
     'port': process.env.PORT || 3000
 }
